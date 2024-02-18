ce1adac116a0cc87686cc25aa4c00b0871b0578d
Yarik Zaychenko <yarikyarik20017@gmail.com>
2024-02-18 21:31:06 +0200

    работает, но с косметическими ошибками

--- WinFormsControlLibrary1/ConverterTxtBox.cs
+++ WinFormsControlLibrary1/ConverterTxtBox.cs
@@ -60,7 +60,6 @@
         {
             InchChanged?.Invoke(this, EventArgs.Empty);
         }
-
         protected virtual void OnErrorOccurred(Exception ex)
         {
             ErrorOccurred?.Invoke(this, new ErrorEventArgs(ex));
@@ -72,22 +71,28 @@
             {
                 TextBox textBox = sender as TextBox;
 
-                if (e.KeyChar == '-' && (textBox.Text.Length == 0 || textBox.SelectionStart != 0))
+                // Проверка на ввод минуса в начале строки
+                if (e.KeyChar == '-' && textBox.SelectionStart == 0)
                 {
-                    e.Handled = true;
-                    throw new ArgumentException("Negative numbers are not allowed.");
+                    e.Handled = true; // Отменяем ввод
+                    throw new Exception("Negative numbers are not allowed.");
                 }
 
+
+                // Перевірка, чи є введений символ цифрою, крапкою або комою, або клавішею Backspace
                 if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.' && e.KeyChar != ',' && e.KeyChar != 'E' && e.KeyChar != '+' && e.KeyChar != '-')
                 {
+                    // Якщо введений символ не є цифрою, крапкою, комою або Backspace, скасувати введення
                     e.Handled = true;
-                    throw new ArgumentException("Attempt of invalid input was stopped");
+                    throw new Exception("Atemp of invalid input was stopped");
                 }
 
+                // Перевірка, чи введений символ - крапка або кома, і чи вони вже присутні у тексті
                 if ((e.KeyChar == '.' || e.KeyChar == ',') && ((sender as TextBox).Text.IndexOf('.') > -1 || (sender as TextBox).Text.IndexOf(',') > -1))
                 {
+                    // Якщо введено більше однієї крапки або коми, скасувати введення
                     e.Handled = true;
-                    throw new ArgumentException("More than one separator was typed!");
+                    throw new Exception("More than one separator was typed!");
                 }
             }
             catch (Exception ex) { OnErrorOccurred(ex); }
@@ -95,19 +100,23 @@
 
         private void ConverterTxtBox_Resize(object sender, EventArgs e)
         {
+            // Размеры компонента изменились, пересчитываем размеры и позиции внутренних элементов
             UpdateInternalElements();
         }
-
         private void UpdateInternalElements()
         {
+            // Определяем высоту текстового поля как 1/3 высоты компонента
             int textBoxHeight = this.Height / 3;
 
+            // Устанавливаем размер и позицию для лейблов
             labelCM.Location = new Point((this.Width - labelCM.Width) / 2, 5);
             labelInch.Location = new Point((this.Width - labelInch.Width) / 2, textBoxHeight + 10);
 
+            // Устанавливаем размеры для текстовых полей
             textBoxCM.Size = new Size(this.Width, textBoxHeight);
             textBoxInch.Size = new Size(this.Width, textBoxHeight);
 
+            // Устанавливаем позиции для текстовых полей
             textBoxCM.Location = new Point(0, labelCM.Bottom);
             textBoxInch.Location = new Point(0, labelInch.Bottom + 5);
         }
@@ -115,35 +124,41 @@
         private void textBox_TextChanged(object sender, EventArgs e)
         {
             TextBox textBox = (TextBox)sender;
-            TextChanges(textBox);
-        }
 
-        private void TextChanges(TextBox textBox)
-        {
-            if (string.IsNullOrWhiteSpace(textBox.Text))
+            try
             {
-                return;
-            }
+                // Попробуйте распарсить введенное значение как число
+                double value = double.Parse(textBox.Text);
 
-            if (!double.TryParse(textBox.Text, out double value))
-            {
-                OnErrorOccurred(new FormatException("Invalid input. Please enter a valid number."));
-                return;
-            }
+                // Проверяем, если число меньше 0, выводим сообщение об ошибке
+                if (value < 0)
+                {
+                    throw new Exception("Negative numbers are not allowed.");
+                }
 
-            if (value < 0)
-            {
-                OnErrorOccurred(new ArgumentException("Negative numbers are not allowed."));
-                return;
+                // Обработка для поля CM
+                if (textBox == textBoxCM)
+                {
+                    CM = value;
+                }
+                // Обработка для поля Inch
+                else if (textBox == textBoxInch)
+                {
+                    Inch = value;
+                }
             }
-
-            if (textBox == textBoxCM)
+            catch (FormatException)
             {
-                CM = value;
+                // Выводим сообщение об ошибке, если введенное значение не является числом
+                OnErrorOccurred(new Exception("Invalid input. Please enter a valid number."));
             }
-            else if (textBox == textBoxInch)
+            catch (Exception ex)
             {
-                Inch = value;
+                // Выводим сообщение об ошибке только если это не отрицательное число
+                if (ex.Message != "Negative numbers are not allowed.")
+                {
+                    OnErrorOccurred(ex);
+                }
             }
         }
     }
